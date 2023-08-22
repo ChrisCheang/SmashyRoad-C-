7d4f381cf66cbb634470f1d561dc1fc863d89aea
ChrisCheang <128516153+ChrisCheang@users.noreply.github.com>
2023-08-22 16:43:47 +0100

    Update Testing Venue.cpp
    Saturation is key

--- Testing Venue.cpp
+++ Testing Venue.cpp
@@ -224,22 +224,22 @@ int main() {
 		Mat imgGrayBox, imgBlurBox, edges;
 		Mat maskPre, mask, masked;
 		cvtColor(imgBox, maskPre, cv::COLOR_BGR2HSV);
-		inRange(maskPre, Scalar(0, 0, 130), Scalar(179, 255, 255), mask);
+		inRange(maskPre, Scalar(0, 120, 150), Scalar(179, 255, 255), mask);
 		bitwise_and(imgBox,imgBox, masked, mask=mask);
-		cvtColor(imgBox, imgGrayBox, cv::COLOR_BGR2GRAY); // it seems for c++ the target image also needs to be specified) // inRange(imgBox, Scalar(45, 100, 200), Scalar(75, 130, 200), imgBox);
+		cvtColor(masked, imgGrayBox, cv::COLOR_BGR2GRAY); // it seems for c++ the target image also needs to be specified) // inRange(imgBox, Scalar(45, 100, 200), Scalar(75, 130, 200), imgBox);
 		GaussianBlur(imgGrayBox, imgBlurBox, Size(3, 3), 0);
 		Canny(imgBlurBox, edges, 100, 200);    // original thresholds 100, 200
 		vector<Vec4i> lines;
 		HoughLinesP(edges, lines, 1, CV_PI / 180, 1, 0, 0);
 		
-		//imshow("ha", imgBox);
-		//moveWindow("ha", 1000, 200);
+		imshow("ha", imgBox);
+		moveWindow("ha", 1000, 200);
 
 		imshow("after inRange", masked);
 		moveWindow("after inRange", 1000, 300);
 
-		//imshow("Canny of small box", edges);
-		//moveWindow("Canny of small box", 1000, 500);
+		imshow("Canny of small box", edges);
+		moveWindow("Canny of small box", 1000, 500);
 
 		//
 
